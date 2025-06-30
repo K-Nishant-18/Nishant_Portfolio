a6daf4cee6caaaea72c219f3300c51434ec85994
KUMAR NISHANT <me.knishant@gmail.com>
2025-07-01 03:35:35 +0530

    Update Loader.tsx

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,7 +4,7 @@ const Loader: React.FC = () => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black dark:bg-black">
       <img
-        src="/public/loader.gif"
+        src="/loader.gif"
         alt="Loading..."
         className="w-30 h-30 sm:w-32 sm:h-32 md:w-80 md:h-80 object-contain"
       />
